25cb260559b37d7f48ecff53dca8fb8d0844378f
hamedhh <hamed_71hh@yahoo.com>
2019-06-02 15:37:32 +0430

    اتمام بخش مربوط به گالری تصاویر

--- MyEShoping/Areas/Admin/Controllers/ProductsController.cs
+++ MyEShoping/Areas/Admin/Controllers/ProductsController.cs
@@ -50,7 +50,7 @@ namespace MyEShoping.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProductID,Title,ShortDescription,Text,Price,ImageName,CreateDate")] Products products,List<int> selectedGroups, HttpPostedFileBase ImageUploaders, string tags)
+        public ActionResult Create([Bind(Include = "ProductID,Title,ShortDescription,Text,Price,ImageName,CreateDate")] Products products, List<int> selectedGroups, HttpPostedFileBase ImageUploaders, string tags)
         {
             if (selectedGroups == null)
             {
@@ -61,8 +61,8 @@ namespace MyEShoping.Areas.Admin.Controllers
             products.ImageName = "index.png";
             if (ImageUploaders != null && ImageUploaders.IsImage())
             {
-                products.ImageName = Guid.NewGuid().ToString() +System.IO.Path.GetExtension(ImageUploaders.FileName);
-                ImageUploaders.SaveAs(Server.MapPath("/Areas/Admin/Images/ProductImages/") +products.ImageName);
+                products.ImageName = Guid.NewGuid().ToString() + System.IO.Path.GetExtension(ImageUploaders.FileName);
+                ImageUploaders.SaveAs(Server.MapPath("/Areas/Admin/Images/ProductImages/") + products.ImageName);
                 ImageResizer img = new ImageResizer();
                 img.Resize(Server.MapPath("/Areas/Admin/Images/ProductImages/") + products.ImageName,
                     Server.MapPath("/Areas/Admin/Images/ProductImages/Thumb/") + products.ImageName);
@@ -74,17 +74,17 @@ namespace MyEShoping.Areas.Admin.Controllers
 
                 foreach (var item in selectedGroups)
                 {
-                    var selectedProductGroup = new Prodct_Selected_Groups() { GroupID=item,ProductID=products.ProductID};
+                    var selectedProductGroup = new Prodct_Selected_Groups() { GroupID = item, ProductID = products.ProductID };
                     db.Prodct_Selected_Groups.Add(selectedProductGroup);
                 }
 
                 string[] _tags = tags.Split(',');
                 foreach (var tagItems in _tags)
                 {
-                    db.Product_Tags.Add(new Product_Tags() {ProductID=products.ProductID,Tag=tagItems.Trim() });
+                    db.Product_Tags.Add(new Product_Tags() { ProductID = products.ProductID, Tag = tagItems.Trim() });
                 }
 
-               
+
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -106,7 +106,7 @@ namespace MyEShoping.Areas.Admin.Controllers
             }
             ViewBag.productGroups = db.Product_Groups.ToList();
             ViewBag.selectedGroup = products.Prodct_Selected_Groups.ToList();
-            ViewBag.tags = string.Join(",", products.Product_Tags.Select(a=>a.Tag).ToList());
+            ViewBag.tags = string.Join(",", products.Product_Tags.Select(a => a.Tag).ToList());
             return View(products);
         }
 
@@ -115,22 +115,72 @@ namespace MyEShoping.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProductID,Title,ShortDescription,Text,Price,ImageName,CreateDate")] Products products,List<int> selectedGroups,HttpPostedFileBase ImageUploaders, string tags)
-         {
-            if (selectedGroups == null)
-            {
-                ViewBag.GroupError = true;
-                ViewBag.productGroups = db.Product_Groups.ToList();
-                ViewBag.selectedGroup = products.Prodct_Selected_Groups.ToList();
-                ViewBag.tags = tags;
-                return View(products);
-            }
+        public ActionResult Edit([Bind(Include = "ProductID,Title,ShortDescription,Text,Price,ImageName,CreateDate")] Products products, List<int> selectedGroups, HttpPostedFileBase ImageUploaders, string tags)
+        {
+
+
             if (ModelState.IsValid)
             {
+                if (selectedGroups == null)
+                {
+                    ViewBag.GroupError = true;
+                    ViewBag.productGroups = db.Product_Groups.ToList();
+                    ViewBag.selectedGroup = products.Prodct_Selected_Groups.ToList();
+                    ViewBag.tags = tags;
+                    return View(products);
+                }
+                if (ImageUploaders != null && ImageUploaders.IsImage())
+                {
+                    if (products.ImageName != "index.png")
+                    {
+                        System.IO.File.Delete(Server.MapPath("/Areas/Admin/Images/ProductImages/" + products.ImageName));
+                        System.IO.File.Delete(Server.MapPath("/Areas/Admin/Images/ProductImages/Thumb/" + products.ImageName));
+                    }
+
+                    var imageName = Guid.NewGuid().ToString() + System.IO.Path.GetExtension(ImageUploaders.FileName);
+                    ImageUploaders.SaveAs(Server.MapPath("/Areas/Admin/Images/ProductImages/" + imageName));
+                    ImageResizer img = new ImageResizer();
+
+                    img.Resize(Server.MapPath("/Areas/Admin/Images/ProductImages/" + imageName),
+                        Server.MapPath("/Areas/Admin/Images/ProductImages/Thumb/") + imageName);
+
+                    products.ImageName = imageName;
+                }
+
+
                 db.Entry(products).State = EntityState.Modified;
+                db.Product_Tags.Where(a => a.ProductID == products.ProductID).ToList().ForEach(a => db.Product_Tags.Remove(a));
+                if (!string.IsNullOrEmpty(tags))
+                {
+                    string[] _tages = tags.Split(',');
+                    foreach (var item in _tages)
+                    {
+                        db.Product_Tags.Add(new Product_Tags() {
+                        ProductID=products.ProductID,
+                        Tag=item.Trim()});
+                    }
+                }
+
+                db.Prodct_Selected_Groups.Where(a => a.ProductID == products.ProductID).ToList().ForEach(a => db.Prodct_Selected_Groups.Remove(a));
+                if (selectedGroups != null)
+                {
+                    foreach (var group in selectedGroups)
+                    {
+                        db.Prodct_Selected_Groups.Add(new Prodct_Selected_Groups()
+                        {
+                            ProductID = products.ProductID,
+                            GroupID = group
+                        });
+                    }
+                }
+                
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
+            ViewBag.productGroups = db.Product_Groups.ToList();
+            ViewBag.selectedGroup = products.Prodct_Selected_Groups.ToList();
+            ViewBag.tags = tags;
             return View(products);
         }
 
@@ -168,5 +218,46 @@ namespace MyEShoping.Areas.Admin.Controllers
             }
             base.Dispose(disposing);
         }
+
+        public ActionResult Gallery(int id)
+        {
+            ViewBag.ProductGalleries = db.Product_Galleries.Where(a => a.ProductID == id).ToList();
+            return View(new Product_Galleries() { ProductID=id});
+        }
+
+        [HttpPost]
+        public ActionResult Gallery(Product_Galleries _productGaleeries,HttpPostedFileBase ImageUp)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ImageUp != null && ImageUp.IsImage())
+                {
+                    _productGaleeries.ImageName =Guid.NewGuid().ToString()+ System.IO.Path.GetExtension(ImageUp.FileName);
+                    ImageUp.SaveAs(Server.MapPath("/Areas/Admin/Images/ProductImages/"+ _productGaleeries.ImageName));
+                    ImageResizer img = new ImageResizer();
+                    img.Resize(Server.MapPath("/Areas/Admin/Images/ProductImages/" + _productGaleeries.ImageName),
+                        Server.MapPath("/Areas/Admin/Images/ProductImages/Thumb/") + _productGaleeries.ImageName);
+                    db.Product_Galleries.Add(_productGaleeries);
+                    db.SaveChanges();
+                }
+            }
+            //return RedirectToAction("Gallery", new { id = galleries.ProductID });
+            ViewBag.ProductGalleries = db.Product_Galleries.Where(a => a.ProductID == _productGaleeries.ProductID).ToList();
+            return View();
+        }
+
+        public ActionResult DeleteGallery(int id)
+        {
+            var gallery = db.Product_Galleries.Find(id);
+            var imgName = db.Product_Galleries.Where(a => a.GalleryID == id).ToList().Select(a => a.ImageName).FirstOrDefault();
+            System.IO.File.Delete(Server.MapPath("/Areas/Admin/Images/ProductImages/" + imgName));
+            System.IO.File.Delete(Server.MapPath("/Areas/Admin/Images/ProductImages/Thumb/" + imgName));
+            db.Product_Galleries.Remove(gallery);
+            db.SaveChanges();
+            ViewBag.ProductGalleries = db.Product_Galleries.Where(a => a.ProductID == gallery.ProductID).ToList();
+            return View("Gallery");
+        }
+
+
     }
 }
