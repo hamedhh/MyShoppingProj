a478f91afaab3b9852382bde11c13f0ee0e94fec
hamedhh <hamed_71hh@yahoo.com>
2019-07-06 16:21:12 +0430

    تکمیل نهایی بخش شبد خرید در ویو اصلی

--- DataLayer/ViewModels/ShopCardViewModel.cs
+++ DataLayer/ViewModels/ShopCardViewModel.cs
@@ -17,5 +17,6 @@ namespace DataLayer.ViewModels
         public int ProductID { get; set; }
         public int Count { get; set; }
         public string ImageName { get; set; }
+        public string Title { get; set; }
     }
 }
--- MyEShoping/Controllers/ShopCardController.cs
+++ MyEShoping/Controllers/ShopCardController.cs
@@ -4,17 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DataLayer.ViewModels;
+using System.Web.Http;
+using DataLayer;
 
 namespace MyEShoping.Controllers
 {
     public class ShopCardController : Controller
     {
+        EShoping_DBEntities _db = new EShoping_DBEntities();
         //ShopCardViewModel
         // GET: ShopCard
         public ActionResult Index()
         {
-            //List<>
-            return View();
+            List<ShopCardViewModel> _cardViewModel = new List<ShopCardViewModel>();
+            if (HttpContext.Session["ShopCard"] != null)
+            {
+                List<ShopCardItem> _cardItem = HttpContext.Session["ShopCard"] as List<ShopCardItem>;
+                if (_cardItem.Count > 0)
+                {
+                    foreach (var item in _cardItem)
+                    {
+                        var product = _db.Products.Where(a => a.ProductID == item.ProductID).Select(c => new {c.Title, c.ImageName }).Single();
+
+                        _cardViewModel.Add(new DataLayer.ViewModels.ShopCardViewModel()
+                        {
+                            Count = item.Count,
+                            ImageName = product.ImageName,
+                            ProductID = item.ProductID,
+                            Title = product.Title
+                        });
+                    }
+ 
+                }
+
+            }
+            return PartialView(_cardViewModel);
         }
     }
 }
